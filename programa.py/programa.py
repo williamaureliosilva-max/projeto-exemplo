5f304800a3ef47b26b1c9dd0d4ce7ed378fbacb8
williamsilva <williamaureliosilva@gmail.com>
2026-05-01 20:09:52 +0100

    Update programa.py
    Tarefa 2 e 3: Melhoria no código e Pull Request

--- programa.py/programa.py
+++ programa.py/programa.py
@@ -1,6 +1,13 @@
-# Programa para comparar se dois números são iguais ou diferentes
+# MELHORIA: Adicionada uma saudação inicial
+print("--- Simulador de Comparação de Números ---")
+
 num1 = int(input("Introduz um número: "))
 num2 = int(input("Introduz outro número: "))
 
 if num1 == num2:
     print("Os números introduzidos são iguais.")
+else:
+    print("Os números introduzidos são diferentes.")
+
+# MELHORIA: Mensagem de encerramento
+print("Obrigado por usar o programa!")
