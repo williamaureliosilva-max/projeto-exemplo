e336cc44043c506e1a961a606577e80f3ce1cc6e
williamsilva <williamaureliosilva@gmail.com>
2026-05-01 18:37:02 +0100

    Tarefa 3: Adicionada mensagem para números diferentes

---
+++ programa.py/programa2.py
@@ -0,0 +1,7 @@
+num1 = int(input("Introduz um número: "))
+num2 = int(input("Introduz outro número: "))
+
+if num1 == num2:
+    print("Os números introduzidos são iguais.")
+else:
+    print("Os números introduzidos são diferentes.")
